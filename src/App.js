bc514638c332e788ccc1ecb1033ff0ac1882fd68
imvsfnd <imvsfnd@gmail.com>
2019-04-12 17:52:50 +0800

    first practice: creat a Counter

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 1,
+  };
+  addCount = () => {
+    this.setState({
+      count: this.state.count+1
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.addCount}>Add</button>
       </div>
     );
   }
