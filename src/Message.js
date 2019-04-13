220a7920332d8a5eedf15f7c9b8d44d3a9586ed1
imvsfnd <imvsfnd@gmail.com>
2019-04-13 20:49:30 +0800

    State Change files

---
+++ src/Message.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Message extends Component {
+  state = {
+    title: 'State Change',
+    text: 'Say Hi',
+  }
+  sayHi = () => {
+    this.setState({
+      text: 'Hi Yo',
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h3>{this.state.text}</h3>
+        <button onClick={this.sayHi}>Change</button>
+      </div>
+    )
+  }
+}
