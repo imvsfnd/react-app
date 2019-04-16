7be48c368eea280c22f3ab5103475ca10c6832aa
imvsfnd <imvsfnd@gmail.com>
2019-04-16 14:05:10 +0800

    Class: 透過toggle與三元運算式來切換viible? ture: false - 完整寫法

---
+++ src/ImgShow.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class imgShow extends Component {
+    state = {
+        visible: true,
+    }
+    toggle = () => {
+        this.setState({
+            visible: !this.state.visible,
+        });
+    };
+    render() {
+        const { visible } = this.state;
+        return (
+            <div>
+                <button onClick={this.toggle}>Toggle</button>
+                {visible ? <img src="imgs\imgShow.jpg" alt="imgShow" /> : null}
+            </div>
+        );
+    }
+}
+
+export default imgShow;
