7bb4cde48aaec2eb13b8c0e935e896819a408950
kmon25 <kmon25@gmail.com>
2019-05-27 10:56:31 +0800

    HOC範例練習過程[僅完成第一個範例]

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Promotion from './Promotion'
+import CreditCards from './CreditCards'
+import withOpen from './hocs/withOpen'
 
-class App extends Component {
-  state = {
-    count: 1,
-  }
-  onClick() {
+const PromotionWithOpen = withOpen(Promotion)
+const CreditCardWithOpen = withOpen(CreditCards)
 
-  }
-  render() {
-    return (
-      <div className="App">
-        <img src="{logo}" className="App-logo" alt="logo" />
-        <input type="text" />
-        <input type="checkbox" id="check" onChange />
-        <label htmlFor="check"></label>
-        <button onClick={this.onClick}>{this.state.count}</button>
-      </div>
-    );
-  }
+const App = () => {
+   <div>
+      <PromotionWithOpen />
+      <CreditCardsWithOpen />
+   </div>
+
+
 }
 
-export default App;
-
-
-/*
-1.must close <a href=""></a>, <p></p>
-2.self close <input type="type"/>, <img src="" alt=""/>, <div class=""/>
-3.駝峰式命名 class/For > className/htmlFor
-4.駝峰式命名 onchange / onclick > onChange/onClick
-5.onClick={值或表達式}
-*/
+export default App
